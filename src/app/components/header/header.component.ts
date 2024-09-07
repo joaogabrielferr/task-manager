57a68275ed2a5fa9f019cbbe27f310f084e557f0
Joao Gabriel <joaogabrielferr@gmail.com>
2024-09-07 11:41:41 -0300

    feat: angular material, basic layout and dark theme

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  @Output()
+  toggleMenu = new EventEmitter();
+
+  _toggleMenu(){
+    this.toggleMenu.emit();
+  }
+
+}
